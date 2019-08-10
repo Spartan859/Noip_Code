37ef260945fa5fc55b347893b5704b4b042c3af9
Spartan859 <36628376+Spartan859@users.noreply.github.com>
2019-08-10 13:45:06 +0800

    nothing

--- 实验舱noip2019/contest7-AC/B-Manacher.cpp
+++ 实验舱noip2019/contest7-AC/B-Manacher.cpp
@@ -18,18 +18,12 @@ ll p[N_MAX*2];
 inline void Manacher(){
 	for(ll i=0,cnt=1;i<n;i++,cnt+=2) sx[cnt]='#',sx[cnt+1]=s[i];
 	sx[0]='$';sx[n*2+1]='#';
-	//for(ll i=0;i<=n*2+1;i++) cout<<sx[i];
-	//cout<<endl;
 	n=n*2+1;
-	//cout<<1<<endl;
 	ll id=0,mx=0;//p »ØÎÄ°ë¾¶ 
 	for(ll i=1;i<=n;i++){
-		
 		if(mx>i) p[i]=min(p[id*2-i],mx-i);
 		else p[i]=1;
-		//dbg(p[i]); 
 		while(sx[i+p[i]]==sx[i-p[i]]) p[i]++;
-		//cout<<p[i]<<endl;
 		if(p[i]+i>mx) id=i,mx=i+p[i];
 		ans[p[i]+i-2]=max(ans[p[i]+i-2],p[i]-1);
 	}
@@ -37,7 +31,6 @@ inline void Manacher(){
 int main(){
 	n=read();scanf("%s",s);
 	Manacher();
-	//for(ll i=1;i<=n*2+1;i++) cout<<p[i]<<' ';
 	for(ll i=n-1;i>=2;i-=2) ans[i]=max(ans[i],ans[i+2]-2); 
 	for(ll i=2;i<=n-1;i+=2) printf("%lld\n",ans[i]);
 	return 0;
